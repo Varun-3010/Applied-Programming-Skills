f85f5e987f40a2e307847ca27f6c5d34c1d5a2b7
Varun-3010 <vtu26575@veltech.edu.in>
2026-02-19 11:12:00 +0530

    Time: 3 ms (99.63%), Space: 50.3 MB (10.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,34 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
-        HashMap<Integer, Integer> freq = new HashMap<>();
-
-        // Count frequency
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        int[] vals = new int[20001];
+        int maxFreq = 0; // At most N, n <= 100000
+        // Keep track of frequency by shifting range
+        for (int number : nums) {
+            int val = 10000 - number;
+            vals[val]++;
+            maxFreq = Math.max(vals[val], maxFreq);
         }
 
-        // Min heap based on frequency
-        PriorityQueue<Integer> minHeap =
-                new PriorityQueue<>((a, b) -> freq.get(a) - freq.get(b));
+        ArrayList<Integer>[] freqToValue = new ArrayList[maxFreq + 1];
+        for (int i = 0; i < vals.length; i++) {
+            if (vals[i] == 0) continue; // 0 occurences
+            int val = 10000 - i;
+            if (freqToValue[vals[i]] == null) freqToValue[vals[i]] = new ArrayList<Integer>();
+            freqToValue[vals[i]].add(val);
+        }
 
-        // Keep only top k elements in heap
-        for (int key : freq.keySet()) {
-            minHeap.add(key);
-
-            if (minHeap.size() > k) {
-                minHeap.poll();
+        Queue<Integer> result = new LinkedList<>();
+        for (int i = 0; i < freqToValue.length; i++) {
+            if (freqToValue[i] == null) continue;
+            for (int num : freqToValue[i]) {
+                result.offer(num);
+                if (result.size() > k) result.poll();
             }
         }
-
-        // Build result array
-        int[] result = new int[k];
-        int i = 0;
-
-        while (!minHeap.isEmpty()) {
-            result[i++] = minHeap.poll();
-        }
-
-        return result;
+        int[] res = new int[k];
+        int idx = 0;
+        for (int val : result) res[idx++] = val;
+        return res;
     }
 }
