50535b7c129b44050278116d5f47ded1f40fbe56
Varun-3010 <vtu26575@veltech.edu.in>
2026-02-23 10:04:57 +0530

    Time: 2 ms (90.13%), Space: 47.9 MB (56.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,15 @@
-class Solution { 
- 
-    public void moveZeroes(int[] nums) { 
- 
-        int insertPosition = 0; 
- 
-        for (int i = 0; i < nums.length; i++) { 
-            if (nums[i] != 0) { 
-                nums[insertPosition++] = nums[i]; 
-            } 
-        } 
- 
-        while (insertPosition < nums.length) { 
-            nums[insertPosition++] = 0; 
-        } 
-    } 
+class Solution {
+     public void moveZeroes(int[] nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
+        }
+    }
 }
