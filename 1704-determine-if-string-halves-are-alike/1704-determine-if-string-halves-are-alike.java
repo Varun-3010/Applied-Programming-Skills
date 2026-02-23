53e1ea27e8daa5bde3c4319ed2cde80e716d38ff
Varun-3010 <vtu26575@veltech.edu.in>
2026-02-23 09:46:56 +0530

    Time: 1 ms (99.75%), Space: 43.1 MB (66.94%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,21 +1,31 @@
 class Solution {
-    public boolean halvesAreAlike(String s) {
-        s=s.toLowerCase();
-        int len=s.length();
-        int vowel=0;
-        char c;
-        for(int i=0;i<len/2;i++){
-            c=s.charAt(i);
-          if(c=='a' || c=='e' || c=='i'|| c=='o'||c=='u'){
-            vowel++;
-          }
-        } for(int i=len/2;i<len;i++){
-            c=s.charAt(i);
-          if(c=='a' || c=='e' || c=='i'|| c=='o'||c=='u'){
-            vowel--;
-          }
-         
+    static{
+        for(int i=0;i<200;i++){
+            halvesAreAlike(" ");
         }
-         return vowel==0;
+    }
+    public static boolean halvesAreAlike(String s) {
+        s = s.toLowerCase();
+        int n = s.length()/2;
+        int fvowel = 0;
+        int svowel = 0;
+        for(int i=0;i<n;i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == 'a'||ch == 'e'||ch == 'i'||ch == 'o'||ch == 'u')
+            {
+                fvowel++;
+            }
+        }
+
+        for(int i=n;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == 'a'||ch == 'e'||ch == 'i'||ch == 'o'||ch == 'u')
+            {
+                svowel++;
+            }
+        }
+        return fvowel == svowel;
     }
 }
