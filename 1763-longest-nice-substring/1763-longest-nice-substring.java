cdfbb678fd517c707f17444e62a6a658325e9dd1
Varun-3010 <vtu26575@veltech.edu.in>
2026-04-15 10:57:03 +0530

    Time: 2 ms (84.78%), Space: 43.8 MB (70.33%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.java
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -1,37 +1,22 @@
 class Solution {
     public String longestNiceSubstring(String s) {
-        String ans="";
-        int maxlen=0;
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                String sub=s.substring(i,j+1);
-                if(niceString(s.substring(i,j+1))){
-                    int len=sub.length();
-                    if(len>maxlen){
-                        maxlen=len;
-                        ans=sub;
-                    }
-                }
-                
-            }
-        }
-        return ans;
-    }
-    public static boolean niceString(String s){
-        HashSet<Character> set=new HashSet<>();
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            set.add(s.charAt(i));
-        }
-        for(int i=0;i<n;i++){
-            if(Character.isLowerCase(s.charAt(i)) && !set.contains(Character.toUpperCase(s.charAt(i)))){
-                return false;
-            }
-            if(Character.isUpperCase(s.charAt(i)) && !set.contains(Character.toLowerCase(s.charAt(i)))){
-                return false;
-            }
-        }
-        return true;
-    }
+       int l = s.length();
+        HashSet<Character> hs = new HashSet<>();
 
+        for(int i=0; i< l; i++){
+hs.add(s.charAt(i));
+        } 
+
+        for(int i =0; i< l;i++){
+            char ch =s.charAt(i);
+            if(hs.contains(Character.toUpperCase(ch)) && hs.contains(Character.toLowerCase(ch))){
+                continue;
+            }
+            String s1 = longestNiceSubstring(s.substring(0,i));
+            String s2 = longestNiceSubstring(s.substring(i+1));
+     
+     return s1.length() >= s2.length()?s1:s2;
+        }
+        return s;
+    }
 }
